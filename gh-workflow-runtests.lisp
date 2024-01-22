45f5627e8a6d23bb72801e5bde7f0bdebfa5f271
Greg Czerniak <gregczrk@gmail.com>
2024-01-22 11:30:43 -0500

    Added code to find common-lisp path

--- gh-workflow-runtests.lisp
+++ gh-workflow-runtests.lisp
@@ -3,7 +3,10 @@
 (asdf:initialize-source-registry
   (list
    :source-registry
-   (list :tree "../common-lisp")
+   (list :tree (merge-pathnames
+                "common-lisp"
+                (uiop:pathname-parent-directory-pathname
+                 (uiop/os:getcwd))))
    :inherit-configuration))
 
 (asdf:test-system :cl-uniquifier)
